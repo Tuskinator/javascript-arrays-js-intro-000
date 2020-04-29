94f310b0b42c5e2e0edb594c85c81874648cb8e0
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-04-29 14:33:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ["Snickers", "Socks", "Kitkat"]
+function addElementToBeginningOfArray(element, array) {
+  var newarray = [element, ...array]
+  return newarray
+}
+function destructivelyAddElementToBeginningOfArray(elemet, array) {
+  
+}
