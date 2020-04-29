b4806bf5b58bf2807afcb3616e559316d9b41217
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-04-29 14:36:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,14 @@ function addElementToBeginningOfArray(element, array) {
   return newarray
 }
 function destructivelyAddElementToBeginningOfArray(elemet, array) {
-  
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(element, array) {
+  var newarray = [...array, element]
+  return newarray
+}
+function destructivelyAddElementToEndOfArray(element, array) {
+  array.push(element)
+  return array
 }
